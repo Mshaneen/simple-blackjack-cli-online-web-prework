6be4480f9d1d55a4968a3c1a60bc4866511a4b02
Shaneen Muhammad <mshaneen.sm@gmail.com>
2019-06-06 17:52:18 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -19,11 +19,11 @@ def get_user_input
 end
 
 def end_game(card_total)
-  puts "Sorry, you hit #{card_total}"
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
-  # code #initial_round here
+  
 end
 
 def hit?
